501d6cc2b77bad395b6f34946cd99b7ed4daab06
damianoconnor04 <damianoconnor04@gmail.com>
2023-10-22 16:59:39 -0400

    add month/year dropdowns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 import React, { useState } from 'react'
-import { IoCaretDown, IoCaretForward, IoChevronBack, IoChevronForward } from 'react-icons/io5'
-import { addMonths, format, startOfMonth, startOfWeek } from 'date-fns'
+import { IoCaretDown, IoChevronBack, IoChevronForward } from 'react-icons/io5'
+import { format, getYear, setYear, startOfMonth, startOfWeek } from 'date-fns'
 import RenderCalendar from './components/RenderCalendar'
-import Dropdown from 'react-dropdown'
+import Dropdown, { Option } from 'react-dropdown'
 import 'react-dropdown/style.css'
 
 const Calendar: React.FC = () => {
@@ -19,11 +19,20 @@ const Calendar: React.FC = () => {
     setCurrentWeek(newDate)
   }
 
-  const options = [
-    format(new Date(), 'MMMM yyyy'),
-    format(addMonths(new Date(), 1), 'MMMM yyyy'),
-    format(addMonths(new Date(), 2), 'MMMM yyyy')
-  ]
+  const monthOptions = Array.from({ length: 12 }, (_, i) => {
+    const date = new Date()
+    date.setMonth(i)
+    return format(date, 'MMMM')
+  })
+
+  const yearOptions = Array.from({ length: 10 }, (_, i) => {
+    const date = new Date()
+    date.setFullYear(date.getFullYear() + i)
+    return format(date, 'yyyy')
+  })
+
+  const handleMonthChange = (e: Option) => setCurrentWeek(startOfMonth(new Date(e.value + getYear(currentWeek))))
+  const handleYearChange = (e: Option) => setCurrentWeek(setYear(startOfMonth(new Date(currentWeek)), parseInt(e.value)))
   
   return (
     <>
@@ -33,20 +42,28 @@ const Calendar: React.FC = () => {
           <button className='p-1.5 text-2xl text-neutral-700 rounded-full hover:bg-neutral-300/20 transition-colors' onClick={handleNextWeek}><IoChevronForward /></button>
         </div>
 
-        <Dropdown 
-          arrowClosed={<IoCaretDown />}
-          arrowOpen={<IoCaretDown />} 
-          controlClassName='!border-none !bg-transparent !text-neutral-600 text-xl font-medium flex items-center gap-2' 
-          className='w-full max-w-xs' 
-          menuClassName='!bg-white/20 !border-none backdrop-blur-sm'
-          options={options} 
-          value={format(currentWeek, 'MMMM yyyy')} 
-          onChange={(e) => setCurrentWeek(startOfMonth(new Date(e.value))) }
-        />
+        <div className='flex gap-2'>
+          <Dropdown
+            arrowClosed={<IoCaretDown className='text-sm' />}
+            arrowOpen={<IoCaretDown className='text-sm' />}
+            controlClassName='!p-0 !py-1 !shadow-none !border-transparent !border-b-gray-300 hover:!border-b-sky-400 transition-colors !text-neutral-600 text-xl font-medium flex items-center gap-3'
+            menuClassName='!border-none !bg-white !max-h-fit'
+            options={monthOptions}
+            value={format(currentWeek, 'MMMM')}
+            onChange={(e) => handleMonthChange(e)}
+          />
+          <Dropdown
+            arrowClosed={<IoCaretDown className='text-sm' />}
+            arrowOpen={<IoCaretDown className='text-sm' />}
+            controlClassName='!p-0 !py-1 !shadow-none !border-transparent !border-b-gray-300 hover:!border-b-sky-400 transition-colors !text-neutral-600 text-xl font-medium flex items-center gap-3'
+            menuClassName='!border-none !bg-white !max-h-fit'
+            options={yearOptions}
+            value={format(currentWeek, 'yyyy')}
+            onChange={(e) => handleYearChange(e)}
+          />
+        </div>
 
-        <button className='text-sky-400 ml-auto font-bold underline underline-offset-2' onClick={() => setCurrentWeek(startOfWeek(new Date()))}>
-          Today
-        </button>
+        <button className='text-sky-400 ml-auto font-bold underline underline-offset-2' onClick={() => setCurrentWeek(startOfWeek(new Date()))}>Today</button>
       </header>
 
       <RenderCalendar currentWeek={currentWeek} />
