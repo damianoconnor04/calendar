87c145f1ab2d3ed572e59c1015569114158da4fe
damianoconnor04 <damianoconnor04@gmail.com>
2023-10-25 16:01:45 -0400

    Change time of events

--- src/app/components/EventModal.tsx
+++ src/app/components/EventModal.tsx
@@ -1,49 +1,50 @@
 import React, { useEffect, useState } from 'react'
 import Dropdown, { Option } from 'react-dropdown'
-import { IoArrowForward, IoClose, IoTimeOutline } from 'react-icons/io5'
+import { IoCaretDown, IoClose, IoInformationCircleOutline, IoRemove, IoTimeOutline } from 'react-icons/io5'
+import { RxDividerVertical } from 'react-icons/rx'
 import Draggable from 'react-draggable'
+import { addDays, format, getMonth } from 'date-fns'
 
 const colors: Color[] = ['bg-sky-400', 'bg-blue-500', 'bg-purple-500', 'bg-pink-500', 'bg-red-500', 'bg-orange-500', 'bg-yellow-500', 'bg-lime-500', 'bg-green-500']
 type Color = 'bg-sky-400' | 'bg-blue-500' | 'bg-purple-500' | 'bg-pink-500' | 'bg-red-500' | 'bg-orange-500' | 'bg-yellow-500' | 'bg-lime-500' | 'bg-green-500'
 
-interface EventModalProps { clickedId: string | null, eventNames: { [key: string]: { name: string, color: Color } }, setEventNames: React.Dispatch<React.SetStateAction<{ [key: string]: { name: string, color: Color } }>>, setEventModal: React.Dispatch<React.SetStateAction<boolean>>, removeEvent: (id: string) => void }
+interface EventModalProps { clickedId: string | null, eventNames: { [key: string]: { name: string, color: Color, start: string, end: string } }, setEventNames: React.Dispatch<React.SetStateAction<{ [key: string]: { name: string, color: Color, start: string, end: string } }>>, setEventModal: React.Dispatch<React.SetStateAction<boolean>>, removeEvent: (start: string, end: string, date: string) => void }
 const EventModal: React.FC<EventModalProps> = ({ clickedId, eventNames, setEventNames, setEventModal, removeEvent }) => {
   if (clickedId === null) return null
   useEffect(() => {
     const savedEventName = localStorage.getItem(clickedId)
     if (savedEventName) setEventNames(prevNames => ({ ...prevNames, [clickedId]: JSON.parse(savedEventName) }))
   }, [clickedId])
+  const [start, end, date] = clickedId.split('-')
+  const [dayOfWeek, month, dayInt] = date.split('/')
+  const generateEventId = (start: string, end: string, date: string) => { return `${start}-${end}-${date}` }
 
   const handleSave = () => {
-    localStorage.setItem(clickedId, JSON.stringify({ name: eventNames[clickedId]?.name, color: eventNames[clickedId]?.color }))
-    setEventModal(false)
+    if (eventNames[clickedId].name) {
+      localStorage.setItem(clickedId, JSON.stringify({ name: eventNames[clickedId]?.name, color: eventNames[clickedId]?.color, start: eventNames[clickedId]?.start, end: eventNames[clickedId]?.end }))
+      setEventModal(false)
+    }
+    else { alert('Name your event before saving') }
   }
   const handleRemove = () => {
     localStorage.removeItem(clickedId)
-    removeEvent(clickedId)
+    removeEvent(start, end, date)
     setEventModal(false)
   }
   const handleModalClose = () => {
     setEventModal(false)
     const savedEvent = localStorage.getItem(clickedId)
-    if (!localStorage.getItem(clickedId)) removeEvent(clickedId)
-    if (savedEvent) if (JSON.parse(savedEvent).name === '') removeEvent(clickedId)
+    if (!localStorage.getItem(clickedId)) removeEvent(start, end, date)
+    if (savedEvent) if (JSON.parse(savedEvent).name === '') removeEvent(start, end, date)
   }
 
   const [eventColor, setEventColor] = useState<Color>('bg-sky-400')
   const setColor = (color: Color) => {
     setEventColor(color)
-    if (clickedId) setEventNames(prevNames => ({ ...prevNames, [clickedId]: { name: eventNames[clickedId]?.name || '', color: color } }))
+    if (clickedId) setEventNames(prevNames => ({ ...prevNames, [clickedId]: { name: eventNames[clickedId]?.name || '', color: color, start: eventNames[clickedId]?.start || '', end: eventNames[clickedId]?.end || '' } }))
   }
 
-  const [hour, dayOfWeek, month, date] = clickedId.split('-')
-  const hourParts: string[] = hour.split(' ')
-  const timestamp: string = hourParts[1]
-  const hourInt: number = parseInt(hourParts[0])
-  let nextHourInt: number = hourInt + 1; if (nextHourInt > 12) nextHourInt = 1
-  const nextHour: string = nextHourInt + ' ' + timestamp
-
-  const handleEventNameChange = (e: React.ChangeEvent<HTMLInputElement>) => setEventNames(prevNames => ({ ...prevNames, [clickedId]: { name: e.target.value, color: eventNames[clickedId]?.color } }))
+  const handleEventNameChange = (e: React.ChangeEvent<HTMLInputElement>) => setEventNames(prevNames => ({ ...prevNames, [clickedId]: { name: e.target.value, color: eventNames[clickedId]?.color, start: eventNames[clickedId]?.start || '', end: eventNames[clickedId]?.end || '' } }))
 
   const eventElem = document.getElementById(`${clickedId}`)?.getBoundingClientRect()
   const containerElem = document.getElementById('dates-container')?.getBoundingClientRect()
@@ -52,17 +53,45 @@ const EventModal: React.FC<EventModalProps> = ({ clickedId, eventNames, setEvent
   let top = undefined
   if (containerElem && eventElem) {
     if (eventElem?.left - 384 < containerElem.width) { left = eventElem?.right - 16 } // no x overflow, go to right
-    if (eventElem?.right + 384 > containerElem.width) { left = eventElem?.left - 368 } // modal width causes x overflow, go to left (minus modal width)
-    if (eventElem?.bottom + 384 < viewportHeight) { top = eventElem?.bottom - 16 } // no y overflow, go to bottom
-    if (eventElem?.bottom + 384 > viewportHeight) { top = eventElem?.top - 368 } // modal height causes y overflow, go to top (minus modal height)
+    if (eventElem?.right + 400 > containerElem.width) { left = eventElem?.left - 368 } // modal width causes x overflow, go to left (minus modal width)
+    if (eventElem?.bottom + 288 < viewportHeight) { top = eventElem?.bottom - 16 } // no y overflow, go to bottom
+    if (eventElem?.bottom + 304 > viewportHeight) { top = eventElem?.top - 288 } // modal height causes y overflow, go to top (minus modal height)
   }
 
-  const hourOptions = Array.from({ length: 24 }, (_, i) => {
-    const hour = i === 0 || i === 12 ? 12 : i % 12;
-    const timestamp = i < 12 ? 'AM' : 'PM';
-    return `${hour} ${timestamp}`
+  const [selectedFirstHour, setSelectedFirstHour] = useState<string>(eventNames[clickedId]?.start || '')
+  const hourOptions = Array.from({ length: 96 }, (_, idx) => {
+    const hour24 = Math.floor(idx / 4)
+    const hour12 = hour24 % 12 === 0 ? 12 : hour24 % 12
+    const minute = (idx % 4) * 15
+    const timestamp = hour24 < 12 ? 'am' : 'pm'
+    return `${hour12}:${minute < 10 ? '0' + minute : minute}${timestamp}`
   })
-  const handleFirstHourChange = (e: Option) => { }
+  const secondHourOptions = () => {
+    const firstHourIdx = hourOptions.indexOf(selectedFirstHour)
+    return hourOptions.slice(firstHourIdx + 1)
+  }
+
+  const handleFirstHourChange = (e: Option) => {
+    setEventNames(prevNames => ({ ...prevNames, [clickedId]: { name: eventNames[clickedId]?.name || '', color: eventNames[clickedId]?.color || '', start: e.value, end: eventNames[clickedId]?.end || '' } }))
+    setSelectedFirstHour(e.value)
+  }
+  const handleSecondHourChange = (e: Option) => setEventNames(prevNames => ({ ...prevNames, [clickedId]: { name: eventNames[clickedId]?.name || '', color: eventNames[clickedId]?.color || '', start: eventNames[clickedId]?.start || '', end: e.value } }))
+
+  const repeatOptions = ['Does not repeat', 'Daily', 'Weekly', 'Weekdays']
+  const monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
+  const monthIndex = monthNames.indexOf(month)
+  const handleRepeatChange = (e: Option) => {
+    const option = e.value
+    const event = eventNames[clickedId]
+    if (option === 'Does not repeat') {}
+    if (option === 'Daily') {
+      for (let i = 1; i <= 7; i++) {
+        const newDate = new Date(new Date().getFullYear(), monthIndex, parseInt(dayInt) + i)
+        const newId = generateEventId(event.start, event.end, format(newDate, 'EEEE/MMMM/dd'))
+        setEventNames(prevNames => ({ ...prevNames, [newId]: event }))
+      }
+    }
+  }
 
   type Type = 'event' | 'task'
   const [type, setType] = useState<Type>('event')
@@ -71,70 +100,96 @@ const EventModal: React.FC<EventModalProps> = ({ clickedId, eventNames, setEvent
     <>
       <div onClick={handleModalClose} className='animate-fade animate-duration-200 absolute inset-0 w-full h-full bg-neutral-600/5' />
       <Draggable handle='.handle' bounds='body'>
-      <div className={`animate-fade animate-duration-200 animate-delay-50 w-full max-w-sm max-h-96 h-full bg-white rounded-xl flex flex-col shadow z-[9999]`} style={{ position: 'absolute', top: `${top}px`, left: `${left}px` }}>
-        <header className='handle cursor-move bg-[#f1f3f4] border-b border-b-[#f1f3f4] flex justify-end rounded-t-xl p-1'>
-          <button onClick={() => handleModalClose()} className='p-0.5 text-xl text-[#73767a] hover:bg-[#e6e8e9] transition-colors rounded-full'><IoClose /></button>
-        </header>
+        <div className={`animate-fade animate-duration-200 animate-delay-50 w-full max-w-sm max-h-72 h-full bg-white rounded-xl flex flex-col shadow z-[9999]`} style={{ position: 'absolute', top: `${top}px`, left: `${left}px` }}>
+          <header className='handle cursor-move bg-[#f1f3f4] border-b border-b-[#f1f3f4] flex justify-end rounded-t-xl p-1'>
+            <button onClick={() => handleModalClose()} className='p-0.5 text-xl text-[#73767a] hover:bg-[#e6e8e9] transition-colors rounded-full'><IoClose /></button>
+          </header>
 
-        <div className='flex flex-1 flex-col p-2'>
-          <label htmlFor={`${hour}`} className='self-end max-w-[calc(90%_-_0.75rem)]'>
-            <input value={eventNames[clickedId].name.toString()} placeholder='New event' onChange={handleEventNameChange} className='pr-16 text-xl w-full border-b-2 border-b-gray-400/70 focus-visible:outline-none focus-visible:border-b-sky-400' />
-          </label>
+          <div className='flex flex-1 flex-col p-2'>
+            <label htmlFor={`${clickedId}`} className='self-end max-w-[calc(90%_-_0.75rem)]'>
+              <input value={eventNames[clickedId].name.toString()} placeholder='New event' onChange={handleEventNameChange} className='pr-16 text-xl w-full border-b-2 border-b-gray-400/70 focus-visible:outline-none focus-visible:border-b-sky-400' />
+            </label>
 
-          <div className='flex items-center pt-3 pb-1 gap-2 ml-[calc(10%_+_0.75rem)]'>
-            <button onClick={() => setType('event')} className={`text-sm transition-colors px-3 py-1.5 rounded-md text-neutral-600 font-medium ${type === 'event' ? 'bg-indigo-100/60 !text-blue-600' : 'hover:bg-neutral-500/10'}`}>Event</button>
-            <button onClick={() => setType('task')} className={`text-sm transition-colors px-3 py-1.5 rounded-md text-neutral-600 font-medium ${type === 'task' ? 'bg-indigo-100/60 !text-blue-600' : 'hover:bg-neutral-500/10'}`}>Task</button>
-          </div>
+            <div className='flex items-center pt-3 pb-1 gap-2 ml-[calc(10%_+_0.75rem)]'>
+              <button onClick={() => setType('event')} className={`text-sm transition-colors px-3 py-1.5 rounded-md text-neutral-600 font-medium ${type === 'event' ? 'bg-indigo-100/60 !text-blue-600' : 'hover:bg-neutral-500/10'}`}>Event</button>
+              <button onClick={() => setType('task')} className={`text-sm transition-colors px-3 py-1.5 rounded-md text-neutral-600 font-medium ${type === 'task' ? 'bg-indigo-100/60 !text-blue-600' : 'hover:bg-neutral-500/10'}`}>Task</button>
+            </div>
 
-          <div className='flex items-center text-sm text-neutral-700 cursor-default py-0.5 gap-2'>
-            <span className='min-w-[10%] max-w-[10%]'>
-              <IoTimeOutline className='text-2xl m-auto' />
-            </span>
+            <div className='flex items-start text-sm text-neutral-700 cursor-default py-0.5 gap-2'>
+              <span className='min-w-[10%] max-w-[10%] mt-1'>
+                <IoTimeOutline className='text-2xl m-auto' />
+              </span>
 
-            <div className='flex items-center w-full justify-between'>
-              <button className='py-0.5 px-1 hover:bg-neutral-500/10 rounded-lg'>{dayOfWeek}, {month} {date}</button>
+              <div className="flex flex-col w-full">
+                <div className='flex items-center gap-0'>
+                  <button className='hover:bg-neutral-500/10 rounded-t-md text-neutral-600 p-1 border-b border-b-transparent hover:!border-sky-400 transition-colors'>
+                    {month + ' ' + dayInt}
+                  </button>
+                  <RxDividerVertical className='text-lg text-neutral-600' />
+                  <div className='flex items-center'>
+                    <button className='hover:bg-neutral-500/10 rounded-t-md transition-colors'>
+                      <Dropdown
+                        arrowClassName='!hidden'
+                        controlClassName='!max-w-fit !bg-inherit !cursor-pointer !p-1 !shadow-none !border-transparent hover:!border-b-sky-400 !transition-colors !text-neutral-600 !rounded-t-md'
+                        className='!max-w-fit'
+                        menuClassName='!border-none !bg-[#FaF9F9] !w-max !overflow-x-hidden !max-h-40'
+                        options={hourOptions}
+                        value={eventNames[clickedId]?.start || ''}
+                        onChange={(e) => handleFirstHourChange(e)}
+                      />
+                    </button>
+                    {type === 'event' && (
+                      <>
+                        <IoRemove />
+                        <button className='hover:bg-neutral-500/10 rounded-t-md transition-colors'>
+                          <Dropdown
+                            arrowClassName='!hidden'
+                            controlClassName='!max-w-fit !bg-inherit !cursor-pointer !p-1 !shadow-none !border-transparent hover:!border-b-sky-400 !transition-colors !text-neutral-600 !rounded-t-md'
+                            className='!max-w-fit'
+                            menuClassName='!border-none !bg-[#FaF9F9] !w-max !overflow-x-hidden !max-h-40'
+                            options={secondHourOptions()}
+                            value={eventNames[clickedId]?.end || ''}
+                            onChange={(e) => handleSecondHourChange(e)}
+                          />
+                        </button>
+                      </>
+                    )}
+                  </div>
+                </div>
 
-              <div className='flex items-center'>
-                <button className='hover:bg-neutral-500/10 rounded-lg transition-colors'>
+                <button className='hover:bg-neutral-500/10 rounded-md transition-colors w-fit'>
                   <Dropdown
-                    arrowClassName='!hidden'
-                    controlClassName='!max-w-fit !bg-inherit !cursor-pointer !px-1 !py-0.5 !shadow-none !border-transparent ring-1 ring-transparent hover:!ring-sky-400 !transition-colors !text-neutral-600 !rounded-lg'
-                    className='!max-w-fit'
-                    menuClassName='!border-none !bg-white !w-max !overflow-x-hidden !max-h-[4.375rem]'
-                    options={hourOptions}
-                    value={hourInt + ' ' + timestamp}
-                    onChange={(e) => handleFirstHourChange(e)}
+                    arrowClosed={<IoCaretDown className='text-xs' />}
+                    arrowOpen={<IoCaretDown className='text-xs' />}
+                    controlClassName='!bg-inherit !cursor-pointer !p-1 !shadow-none !border-transparent transition-colors !text-neutral-600 flex items-center gap-2'
+                    menuClassName='!border-none !bg-[#FaF9F9] !max-h-32 !overflow-x-hidden !w-max'
+                    options={repeatOptions}
+                    value={'Does not repeat'}
+                    onChange={(e) => handleRepeatChange(e)}
                   />
                 </button>
-                <IoArrowForward className='p-0.5 pt-1 text-lg' />
-                <button className='hover:bg-neutral-500/10 rounded-lg transition-colors'>
-                  <Dropdown
-                    arrowClassName='!hidden'
-                    controlClassName='!max-w-fit !bg-inherit !cursor-pointer !px-1 !py-0.5 !shadow-none !border-transparent ring-1 ring-transparent hover:!ring-sky-400 !transition-colors !text-neutral-600 !rounded-lg'
-                    className='!max-w-fit'
-                    menuClassName='!border-none !bg-white !w-max !overflow-x-hidden !max-h-[4.375rem]'
-                    options={hourOptions}
-                    value={nextHour}
-                    onChange={(e) => handleFirstHourChange(e)}
-                  />
-                </button>
+
               </div>
+            </div>
 
+            <div className='flex items-start text-sm text-neutral-700 cursor-default py-0.5 pt-2 gap-2'>
+              <span className='min-w-[10%] max-w-[10%]'>
+                <IoInformationCircleOutline className='text-2xl m-auto' />
+              </span>
+              <textarea placeholder='Add description' className='resize-none w-full bg-[#FaF9F9] p-0.5 px-1.5 rounded-lg focus:outline-none focus:ring-2 focus:ring-sky-400 border border-gray-300' />
             </div>
 
           </div>
 
-        </div>
+          <div className='flex items-center gap-1 ml-auto p-2 pt-0'>
+            {colors.map((color, idx) => <button onClick={() => setColor(color)} key={idx} className={`w-4 h-4 aspect-square rounded-full ${color}`} />)}
+          </div>
 
-        <div className='flex items-center gap-1 ml-[calc(10%_+_1.25rem)] py-2'>
-          {colors.map((color, idx) => <button onClick={() => setColor(color)} key={idx} className={`w-4 h-4 aspect-square rounded-full ${color}`} />)}
+          <footer className='bg-[#f1f3f4] border-t border-t-[#f1f3f4] flex justify-end gap-3 rounded-b-xl p-1'>
+            <button onClick={handleRemove} className='p-0.5 rounded-lg text-rose-400 font-medium text-sm'>Remove</button>
+            <button onClick={handleSave} className='p-0.5 px-3 rounded-lg bg-sky-400 text-white font-medium text-sm'>Save</button>
+          </footer>
         </div>
-
-        <footer className='bg-white border-t border-t-[#f1f3f4] flex justify-end gap-2 rounded-b-xl p-1'>
-          <button onClick={handleRemove} className='py-1 px-3 rounded-lg bg-rose-400 text-white font-medium text-sm'>Delete</button>
-          <button onClick={handleSave} className='py-1 px-3 rounded-lg bg-sky-400 text-white font-medium text-sm'>Save</button>
-        </footer>
-      </div>
       </Draggable>
     </>
   )
--- src/app/components/RenderWeekCalendar.tsx
+++ src/app/components/RenderWeekCalendar.tsx
@@ -9,7 +9,7 @@ type Color = 'bg-sky-400' | 'bg-blue-500' | 'bg-purple-500' | 'bg-pink-500' | 'b
 interface RenderWeekCalendarProps { currentWeek: Date }
 const RenderWeekCalendar: React.FC<RenderWeekCalendarProps> = ({ currentWeek }) => {
   useEffect(() => {
-    const savedEventNames: { [key: string]: { name: string, color: Color} } = {}
+    const savedEventNames: { [key: string]: { name: string, color: Color, start: string, end: string } } = {}
     for (let i = 0; i < localStorage.length; i++) {
       const key = localStorage.key(i)
       if (key) savedEventNames[key] = JSON.parse(localStorage.getItem(key) ?? '')
@@ -18,16 +18,29 @@ const RenderWeekCalendar: React.FC<RenderWeekCalendarProps> = ({ currentWeek }) 
   }, [])
   const [clickedId, setClickedId] = useState<string | null>(null)
   const [eventModal, setEventModal] = useState<boolean>(false)
-  const [eventNames, setEventNames] = useState<{ [key: string]: { name: string, color: Color } }>({})
+  const [eventNames, setEventNames] = useState<{ [key: string]: { name: string, color: Color, start: string, end: string } }>({})
+
+  const generateEventId = (start: string, end: string, date: string) => { 
+    const id = `${start}-${end}-${date}`
+    return id
+  }
 
   const createNewEvent = (hour: string, date: string) => {
-    const id = `${hour}-${date}`
+    const hourParts: string[] = hour.split(' ')
+    const timestamp: string = hourParts[1].toLowerCase()
+    const hourInt: number = parseInt(hourParts[0])
+    let nextHourInt: number = hourInt + 1; if (nextHourInt > 12) nextHourInt = 1
+    const nextHour = nextHourInt + ':00' + timestamp
+
+    const id = generateEventId(hourInt + ':00' + timestamp, nextHour, date)
     setClickedId(id)
-    if (!eventNames[id]) setEventNames(prevNames => ({ ...prevNames, [id]: { name: '', color: 'bg-sky-400' } }))
+
+    if (!eventNames[id]) setEventNames(prevNames => ({ ...prevNames, [id]: { name: '', color: 'bg-sky-400', start: hourInt + ':00' + timestamp, end: nextHour } }))
     setEventModal(true)
   }
 
-  const removeEvent = (id: string) => {
+  const removeEvent = (start: string, end: string, date: string) => {
+    const id = generateEventId(start, end, date)
     setEventNames(prevNames => {
       const newEventNames = { ...prevNames }
       delete newEventNames[id]
@@ -55,11 +68,39 @@ const RenderWeekCalendar: React.FC<RenderWeekCalendarProps> = ({ currentWeek }) 
     return hours
   }
 
+  const getNextHour = (hour: string) => {
+    const hourParts: string[] = hour.split(' ')
+    const timestamp: string = hourParts[1].toLowerCase()
+    const hourInt: number = parseInt(hourParts[0])
+    let nextHourInt: number = hourInt + 1; if (nextHourInt > 12) nextHourInt = 1
+    const nextHour = nextHourInt + ':00' + timestamp
+    return nextHour
+  }
+  const formatHour = (hour: string) => {
+    const hourParts: string[] = hour.split(' ')
+    const timestamp: string = hourParts[1].toLowerCase()
+    return hourParts[0] + timestamp
+  }
+
   const mapDatesToDaysOfWeek = (dates: Date[]): { date: Date; dayOfWeek: string }[] =>
     dates.map((date) => ({ date, dayOfWeek: date.toLocaleDateString(undefined, { weekday: 'short' }) }))
 
   const mapHoursToDaysOfWeek = (hours: Date[]): { hours: Date; hour: string }[] =>
     hours.map((hours) => ({ hours, hour: hours.toLocaleTimeString(undefined, { hour: 'numeric', minute: 'numeric' }) }))
+
+  const calculateCellPosition = (startTime: string, endTime: string) => {
+    const [startHour, startMinute] = startTime.split(':')
+    const [endHour, endMinute] = endTime.split(':')
+    const startHourInt = parseInt(startHour)
+    const endHourInt = parseInt(endHour)
+    const startMinutes = startHourInt === 12 ? 0 + parseInt(startMinute) : startHourInt * 60 + parseInt(startMinute)
+    const endMinutes = endHourInt === 12 ? 0 + parseInt(endMinute) : endHourInt * 60 + parseInt(endMinute)
+    const cellHeight = 100 / 1440
+    const top = startMinutes * cellHeight
+    const height = (endMinutes - startMinutes) * cellHeight
+    return { top, height }
+  }
+
 
   const renderWeekCalendar = () => {
     const dates = generateDates()
@@ -76,7 +117,7 @@ const RenderWeekCalendar: React.FC<RenderWeekCalendarProps> = ({ currentWeek }) 
           </button>
           <ul className='w-full grid grid-cols-7 gap-4 p-4 grid-rows-1'>
             {mappedDates.map(({ date, dayOfWeek }) => (
-              <Link href={{ pathname: '/day', query: { date: date.toISOString() }}} key={date.toISOString()} className={`flex flex-col items-center gap-3 transition-colors ${!isToday(date) && 'hover:!text-slate-900'} ${isToday(date) ? 'text-sky-400' : 'text-neutral-500'}`}>
+              <Link href={{ pathname: '/day', query: { date: date.toISOString() } }} key={date.toISOString()} className={`flex flex-col items-center gap-3 transition-colors ${!isToday(date) && 'hover:!text-slate-900'} ${isToday(date) ? 'text-sky-400' : 'text-neutral-500'}`}>
                 <span className='uppercase text-xs'>{dayOfWeek}</span>
                 <span className='text-2xl font-medium'>{date.getDate()}</span>
               </Link>
@@ -85,27 +126,43 @@ const RenderWeekCalendar: React.FC<RenderWeekCalendarProps> = ({ currentWeek }) 
         </div>
 
         <main className='h-[calc(100%_-_5.75rem)] grid grid-cols-[auto,10fr] overflow-y-scroll hide-scroll'>
-          <ul className='grid grid-rows-24 p-2 max-w-[9ch] min-w-[9ch]'>
+          <ul className='grid grid-rows-24 p-2 max-w-[7ch] min-w-[7ch]'>
             {mappedHours.map(({ hour }) => (
-              <li key={hour} className='ml-auto gap-3 p-2'>
+              <li key={hour} className='ml-auto h-10 max-h-10 min-h-10 flex justify-center items-center'>
                 <span className='uppercase text-xs text-neutral-500'>{hour}</span>
               </li>
             ))}
           </ul>
           <section id='dates-container' className='w-full grid grid-cols-7 grid-rows-24 p-2 overflow-hidden'>
             {mappedDates.map(({ date, dayOfWeek }) => (
-              <div key={dayOfWeek} className='grid grid-cols-1 grid-rows-24 border-r border-gray-300 last:border-none'>
-                {mappedHours.map(({ hour }) => (
-                  <div id={`${hour}-${format(date, 'EEEE-MMMM-dd')}`} key={`${hour}-${date}`} className='border-b border-gray-300 last:border-none'>
-                    <button onClick={() => createNewEvent(hour, format(date, 'EEEE-MMMM-dd'))} className='w-full h-full hover:ring-[1px] hover:ring-sky-400'>
-                      {eventNames[`${hour}-${format(date, 'EEEE-MMMM-dd')}`] && 
-                        <div className={`w-full h-full truncate grid place-items-center text-white font-medium text-sm ${eventNames[`${hour}-${format(date, 'EEEE-MMMM-dd')}`].color}`}>
-                          {eventNames[`${hour}-${format(date, 'EEEE-MMMM-dd')}`].name.toString() !== '' ? eventNames[`${hour}-${format(date, 'EEEE-MMMM-dd')}`].name.toString() : 'New event' }
+              <div key={dayOfWeek} className='grid grid-cols-1 relative grid-rows-24 border-r border-gray-300 last:border-r-0'>
+                {Array.from({ length: 168 }, (_, idx) => { /* overlay horizontal grid for hours */
+                  const row = Math.floor(idx / 7) + 1
+                  const col = (idx % 7) + 1
+                  return (
+                    <div key={idx} className='absolute w-full border-b border-gray-300' style={{
+                      top: `${((row - 1) / 24) * 100}%`,
+                      left: `${((col - 1) / 7) * 100}%`,
+                      height: `${100 / 24}%`,
+                      width: `${100 / 7}%`,
+                    }} />
+                  )
+                })}
+                {mappedHours.map(({ hour }, idx) => {
+                  const id = generateEventId(formatHour(hour), getNextHour(hour), format(date, 'EEEE/MMMM/dd'))
+                  const event = eventNames[id]
+                  if (event) {
+                    const { top, height } = calculateCellPosition(event.start, event.end)
+                    return (
+                      <button id={id} key={id} className='absolute w-full' style={{ top: `${top}%`, height: `${height}%` }} onClick={() => createNewEvent(hour, format(date, 'EEEE/MMMM/dd'))}>
+                        <div className={`w-full h-full truncate grid place-items-center text-white font-medium text-sm ${event.color}`}>
+                          {event.name.toString() !== '' ? event.name.toString() : 'New event'}
                         </div>
-                      }
-                    </button>
-                  </div>
-                ))}
+                      </button>
+                    )
+                  }
+                  return <button id={id} key={id} className='absolute w-full max-h-10' style={{ top: `${idx * 100 / 24}%`, height: '100%' }} onClick={() => createNewEvent(hour, format(date, 'EEEE/MMMM/dd'))} />
+                })}
               </div>
             ))}
             {eventModal && <EventModal clickedId={clickedId} eventNames={eventNames} setEventNames={setEventNames} setEventModal={setEventModal} removeEvent={removeEvent} />}
